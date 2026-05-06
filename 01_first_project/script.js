60cc064db011f47e6609de5556c19da479a9be65
Aasim <royalaasim@gmail.com>
2026-05-06 21:10:04 +0530

    Add initial project files for JavaScript learning exercises

---
+++ 01_first_project/script.js
@@ -0,0 +1,11 @@
+let cart_items = 0;
+document.querySelector("#list").innerText= `My list has ${cart_items} items`;
+
+// let visitCount; 
+// visit = prompt("Have you visited our website before? (yes/no)");
+// visitCount=1; 
+// if (visit.toLowerCase() === "y") {
+//   alert("Welcome back! We're glad to see you again.");
+//   visitCount++;
+//   console.log(`visit count:${visitCount}`);
+// }
